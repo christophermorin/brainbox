c0df362c6b747edb9bac4cdbdb6c751362e2d536
Chris Morin <longhumansbiz@gmail.com>
2023-05-11 14:33:27 -0300

    cleaning up. Nav into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,6 @@
+import NavBar from "@/pages/NavBar/NavBar"
 import TitlePage from "@/pages/Title/Title"
 import Main from "@/pages/Main/Main"
-
-function NavBar() {
-  return (
-    <div className="flex p-2 justify-between">
-      <span>Logo</span>
-      <span>User</span>
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <footer className="flex justify-between items-center p-2 h-16 bg-zinc-900">
-      <span>One</span>
-      <span>Two</span>
-      <span>Three</span>
-    </footer>
-  )
-}
-
-function NewIdea() {
-  return (
-    <div className="flex justify-center items-center h-56 bg-zinc-950 rounded-md">
-      <span className="text-7xl">+</span>
-    </div>
-  )
-}
-
-function Idea() {
-  return (
-    <div className="flex flex-col justify-between items-center p-2 h-56 bg-slate-300 rounded-md text-black">
-      <p>Date and time</p>
-      <p>Title</p>
-      <p>Pages</p>
-
-    </div>
-  )
-}
 
 export default function Home() {
   const isSignedIn = true
---
+++ src/pages/NavBar/NavBar.tsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+  return (
+    <div className="flex p-2 justify-between">
+      <span>Logo</span>
+      <span>User</span>
+    </div>
+  )
+}
